69fb2cf8b9189aed974622e7fdf51699bdfedd10
zhangwl916 <zhangwl_916@163.com>
2019-08-02 18:24:36 +0800

    问答

--- irobot-admin/src/main/java/com/irobot/admin/controller/test.java
+++ irobot-admin/src/main/java/com/irobot/admin/controller/test.java
@@ -2,21 +2,26 @@ package com.irobot.admin.controller;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.http.HttpUtil;
+import org.apdplat.qa.SharedQuestionAnsweringSystem;
+import org.apdplat.qa.model.CandidateAnswer;
+import org.apdplat.qa.model.Question;
 
 import java.util.Date;
+import java.util.List;
 
 public class test {
 
     public static void main(String[] args) {
 
-        Date date = DateUtil.date();
-
-        System.out.println(date);
-
-
-        String result1= HttpUtil.get("https://www.baidu.com/");
-
-        System.out.println(result1);
+        String questionStr = "五大洲是哪五个";
+        Question question = SharedQuestionAnsweringSystem.getInstance().answerQuestion(questionStr);
+        if (question != null) {
+            List<CandidateAnswer> candidateAnswers = question.getAllCandidateAnswer();
+            int i=1;
+            for(CandidateAnswer candidateAnswer : candidateAnswers){
+                System.out.println((i++)+"、"+candidateAnswer.getAnswer()+":"+candidateAnswer.getScore());
+            }
+        }
 
 
 
