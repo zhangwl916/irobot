86fd12a091233af825daf99a7e829375310590f1
zhangwl916 <zhangwl_916@163.com>
2019-11-19 10:56:56 +0800

    东吴

---
+++ irobot-mbg/src/main/java/com/allianity/entity/HolidayYear.java
@@ -0,0 +1,95 @@
+package com.allianity.entity;
+
+import java.io.Serializable;
+
+public class HolidayYear implements Serializable {
+    private String id;
+
+    private String nowDate;
+
+    private String str1;
+
+    private String str2;
+
+    private String beginTime;
+
+    private String endTime;
+
+    private String tip;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNowDate() {
+        return nowDate;
+    }
+
+    public void setNowDate(String nowDate) {
+        this.nowDate = nowDate;
+    }
+
+    public String getStr1() {
+        return str1;
+    }
+
+    public void setStr1(String str1) {
+        this.str1 = str1;
+    }
+
+    public String getStr2() {
+        return str2;
+    }
+
+    public void setStr2(String str2) {
+        this.str2 = str2;
+    }
+
+    public String getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(String beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getTip() {
+        return tip;
+    }
+
+    public void setTip(String tip) {
+        this.tip = tip;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", nowDate=").append(nowDate);
+        sb.append(", str1=").append(str1);
+        sb.append(", str2=").append(str2);
+        sb.append(", beginTime=").append(beginTime);
+        sb.append(", endTime=").append(endTime);
+        sb.append(", tip=").append(tip);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
